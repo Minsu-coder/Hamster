0e7c89ee8d70fc3615fcfcd14109e304c8986d5a
PADA-1-5-10 <PADA-1-5-10@Erxess-iMac-6.local>
2025-12-23 17:01:15 +0800

    to-do-list.html

---
+++ list.js
@@ -0,0 +1,28 @@
+const btn = document.getElementById('nextbutton');
+const box = document.getElementById('list-slide');
+
+btn.addEventListener('click', () => {
+    box.classList.toggle('active');
+});
+
+
+function addlist() {
+    const input = document.getElementById('list');
+    const value = input.value.trim();
+    if (value === '') return;
+
+    const ul = document.getElementById('card-2');
+    
+    const li = document.createElement('li');
+    li.textContent = value;
+    const delBtn = document.createElement('addlist');
+    delBtn.textContent = '';
+    // delBtn.style.marginLeft = '10px';
+    delBtn.addEventListener('click', () =>  li.remove());
+
+    li.appendChild(delBtn);
+    ul.appendChild(li);
+
+
+    input.value = ''
+}
